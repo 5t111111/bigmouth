0b1bfc65355e109d7cdad6a51cedf82885fc8ed9
WAKASUGI 5T111111 <baenej@gmail.com>
2015-01-26 11:29:39 +0900

    Add comment

--- lib/bigmouth.rb
+++ lib/bigmouth.rb
@@ -5,6 +5,8 @@ require "tinymce-rails"
 
 module Bigmouth
   class Application < Rails::Application
+
+    # SanitizeHelper configuration
     config.action_view.sanitized_allowed_tags = %w(h1 h2 h3 h4 h5 h6 p ul ol li a img table tr td em br strong)
     config.action_view.sanitized_allowed_attributes = %w(id class href)
   end
