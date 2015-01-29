ad6853ac35a5fc7ad6653236f61d7f563a17587c
WAKASUGI 5T111111 <baenej@gmail.com>
2015-01-30 01:33:46 +0900

    Add tests for articles_controller

--- app/helpers/bigmouth/application_helper.rb
+++ app/helpers/bigmouth/application_helper.rb
@@ -1,4 +1,14 @@
 module Bigmouth
   module ApplicationHelper
+    # Returns Bootstrap friendly type
+    def alert_class_for(flash_type)
+      {
+        success: 'alert-success',
+        error: 'alert-danger',
+        danger: 'alert-danger',
+        alert: 'alert-warning',
+        notice: 'alert-info'
+      }[flash_type.to_sym] || flash_type.to_s
+    end
   end
 end
--- test/controllers/bigmouth/articles_controller_test.rb
+++ test/controllers/bigmouth/articles_controller_test.rb
@@ -25,37 +25,77 @@ module Bigmouth
     test "should not get new without logging in" do
       get :new
       assert_redirected_to root_path
+      assert flash[:alert].present?
     end
 
-    # test "should create article" do
-    #   assert_difference('Article.count') do
-    #     post :create, article: { text: @article.text, title: @article.title }
-    #   end
+    test "should create article when logging in" do
+      login_user
+      assert_difference('Article.count') do
+        post :create, article: { text: @article.text, title: @article.title }
+      end
 
-    #   assert_redirected_to article_path(assigns(:article))
-    # end
+      assert_redirected_to article_path(assigns(:article))
+    end
 
-    # test "should show article" do
-    #   get :show, id: @article
-    #   assert_response :success
-    # end
+    test "should not create article without logging in" do
+      assert_no_difference('Article.count') do
+        post :create, article: { text: @article.text, title: @article.title }
+      end
 
-    # test "should get edit" do
-    #   get :edit, id: @article
-    #   assert_response :success
-    # end
+      assert flash[:alert].present?
+      assert_redirected_to root_path
+    end
 
-    # test "should update article" do
-    #   patch :update, id: @article, article: { text: @article.text, title: @article.title }
-    #   assert_redirected_to article_path(assigns(:article))
-    # end
+    test "should show article" do
+      get :show, id: @article
+      assert_response :success
+    end
 
-    # test "should destroy article" do
-    #   assert_difference('Article.count', -1) do
-    #     delete :destroy, id: @article
-    #   end
+    test "should get edit when logging in" do
+      login_user
+      get :edit, id: @article
+      assert_response :success
+    end
 
-    #   assert_redirected_to articles_path
-    # end
+    test "should not get edit without logging in" do
+      get :edit, id: @article
+      assert flash[:alert].present?
+      assert_redirected_to root_path
+    end
+
+    test "should update article when logging in" do
+      login_user
+      patch :update, id: @article, article: { text: "New Text", title: "New Title" }
+
+      assert_equal Article.find(@article.id).text, "New Text"
+      assert_equal Article.find(@article.id).title, "New Title"
+      assert_redirected_to article_path(assigns(:article))
+    end
+
+    test "should not update article without logging in" do
+      patch :update, id: @article, article: { text: @article.text, title: @article.title }
+      assert_not_equal Article.find(@article.id).text, "New Text"
+      assert_not_equal Article.find(@article.id).title, "New Title"
+      assert flash[:alert].present?
+      assert_redirected_to root_path
+    end
+
+    test "should destroy article when logging in" do
+      login_user
+      assert_difference('Article.count', -1) do
+        delete :destroy, id: @article
+      end
+
+      assert_redirected_to articles_path
+    end
+
+    test "should not destroy article without logging in" do
+      assert_no_difference('Article.count') do
+        delete :destroy, id: @article
+      end
+
+      assert flash[:alert].present?
+      assert_redirected_to root_path
+    end
   end
 end
