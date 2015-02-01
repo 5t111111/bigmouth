6950bfcda3283bb6b68990fb0653e1751233423b
WAKASUGI 5T111111 <baenej@gmail.com>
2015-02-01 10:47:51 +0900

    WIP

--- app/controllers/bigmouth/admin/articles_controller.rb
+++ app/controllers/bigmouth/admin/articles_controller.rb
@@ -5,7 +5,7 @@ module Bigmouth
 
     layout "bigmouth/admin/application"
 
-    before_action :set_article, only: %i(show edit destroy)
+    before_action :set_article, only: %i(show edit update destroy)
 
     def index
       @articles = Article.all
@@ -21,15 +21,37 @@ module Bigmouth
     def edit
     end
 
+    def create
+      @article = Article.new(article_params)
+      @article.author_id = current_user.id
+
+      if @article.save
+        redirect_to @article, notice: 'Article was successfully created.'
+      else
+        render :new
+      end
+    end
+
+    def update
+      if @article.update(article_params)
+        redirect_to admin_article_path(@article), notice: 'Article was successfully updated.'
+      else
+        render :edit
+      end
+    end
+
     def destroy
-      puts "*" * 50
-      puts "FUCK ME"
-      puts "*" * 50
       @article.destroy
       redirect_to articles_url, notice: 'Article was successfully destroyed.'
     end
 
     private
+      # Only allow a trusted parameter "white list" through.
+      def article_params
+        # params.require(:article).permit(:title, :text, :author_name)
+        params.require(:article).permit(:title, :text)
+      end
+
       # Use callbacks to share common setup or constraints between actions.
       def set_article
         @article = Article.find(params[:id])
--- app/models/bigmouth/article.rb
+++ app/models/bigmouth/article.rb
@@ -2,5 +2,7 @@ module Bigmouth
   class Article < ActiveRecord::Base
     belongs_to :author, class_name: Bigmouth.config.author_class
     validates :title, presence: true
+
+    default_scope { order('created_at DESC') }
   end
 end
